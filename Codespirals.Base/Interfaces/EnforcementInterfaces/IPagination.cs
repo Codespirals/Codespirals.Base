aeedf3ec4e44f35471f5160cd225ce32108d0fb4
Eli <elias.styner@gmail.com>
2024-08-06 23:37:37 +0200

    moved search related interfaces to this project

---
+++ Codespirals.Base/Interfaces/EnforcementInterfaces/IPagination.cs
@@ -0,0 +1,13 @@
+﻿namespace Codespirals.Base
+{
+    /// <summary>
+    /// The required properties to implement a pagination system
+    /// </summary>
+    public interface IPagination : ISearch
+    {
+        /// <summary>
+        /// The total number of search results that matched the parameters
+        /// </summary>
+        int TotalResults { get; }
+    }
+}
---
+++ Codespirals.Base/Interfaces/EnforcementInterfaces/ISearchResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Codespirals.Base
+{
+    /// <summary>
+    /// The result from an <see cref="ISearch"/> query
+    /// </summary>
+    /// <typeparam name="T">The type of the result objects</typeparam>
+    public interface ISearchResult<T> : IPagination
+    {
+        /// <summary>
+        /// The returned search results (max set by <seealso cref="ISearch.Limit"/>)
+        /// </summary>
+        ICollection<T> Results { get; }
+    }
+}
