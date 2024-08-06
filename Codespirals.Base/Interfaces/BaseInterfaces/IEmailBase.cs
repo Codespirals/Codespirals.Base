3169ac86402e1fec2411942831d448abd5d77675
Eli <elias.styner@gmail.com>
2024-08-06 17:30:47 +0200

    added email base

---
+++ Codespirals.Base/Interfaces/BaseInterfaces/IEmailBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Codespirals.Base
+{
+
+    public interface IEmailBase
+    {
+        string From { get; }
+        string To { get; }
+        string Subject { get; }
+        string Body { get; }
+    }
+}
