1b90d34342a139283e26689d42b8be9b54b412a3
Elias Styner <elias.styner@auticon.ch>
2024-08-08 10:20:57 +0200

    added credit to image interface

--- Codespirals.Base/Interfaces/BaseInterfaces/IImageBase.cs
+++ Codespirals.Base/Interfaces/BaseInterfaces/IImageBase.cs
@@ -1,14 +1,14 @@
 ﻿namespace Codespirals.Base
 {
-    public interface IImageBase
+    public interface IImageBase : IHasUrl
     {
-        /// <summary>
-        /// The url that leads to the image
-        /// </summary>
-        public string Url { get; }
         /// <summary>
         /// A short description of what is in the image. Mainly to help people with impaired vision
         /// </summary>
         public string AltText { get; }
+        /// <summary>
+        /// Credit to the entity that made the image
+        /// </summary>
+        public string Credit { get; set; }
     }
 }
---
+++ Codespirals.Base/Interfaces/EnforcementInterfaces/IHasUrl.cs
@@ -0,0 +1,10 @@
+﻿namespace Codespirals.Base
+{
+    public interface IHasUrl
+    {
+        /// <summary>
+        /// The url that leads to the image
+        /// </summary>
+        public string Url { get; }
+    }
+}
