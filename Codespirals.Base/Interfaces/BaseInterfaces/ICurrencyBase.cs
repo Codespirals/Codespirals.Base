0b3c69b3c0dbdee75a507388a25e76a79c92dc65
Eli <elias.styner@gmail.com>
2024-08-26 01:50:56 +0200

    improved keyed api

--- Codespirals.Base/Interfaces/BaseInterfaces/ICurrencyBase.cs
+++ Codespirals.Base/Interfaces/BaseInterfaces/ICurrencyBase.cs
@@ -11,7 +11,7 @@ namespace Codespirals.Base
         /// This is only for display purposes and we can't guarantee conversions at this rate.
         /// The real rate will be set by the payment processor and may therfore fluctuate.
         /// </remarks>
-        public decimal RateToDollar { get; }
+        public decimal RateToUsd { get; }
         /// <summary>
         /// The symbol the currency uses
         /// </summary>
