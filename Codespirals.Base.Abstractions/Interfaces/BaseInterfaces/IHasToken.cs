b78a401161e090a2a27cb850ba9855631d80e2fd
Eli <elias.styner@gmail.com>
2024-10-18 00:59:33 +0200

    moving around some classes

--- Codespirals.Base.Abstractions/Interfaces/BaseInterfaces/IHasToken.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Codespirals.Base
-{
-    public interface IHasToken
-    {
-        public KeyValuePair<string, string> Token { get; }
-    }
-}
---
+++ Codespirals.Base.Abstractions/Interfaces/BaseInterfaces/IToken.cs
@@ -0,0 +1,12 @@
+﻿namespace Codespirals.Base
+{
+    /// <summary>
+    /// A simple token usually for HTTP use
+    /// </summary>
+    /// <remarks>While it would make sense to use a <see cref="KeyValuePair"/>, storing those in a database can cause issues I wish to avoid.</remarks>
+    public interface IToken
+    {
+        public string Key { get; }
+        public string Value { get; }
+    }
+}
