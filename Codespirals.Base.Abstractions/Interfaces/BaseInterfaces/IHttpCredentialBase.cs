c9a012de302c28c5e7086006acae4fb64615b3c7
Eli <elias.styner@gmail.com>
2024-10-17 14:06:42 +0200

    builds

---
+++ Codespirals.Base.Abstractions/Interfaces/BaseInterfaces/IHttpCredentialBase.cs
@@ -0,0 +1,10 @@
+﻿namespace Codespirals.Base
+{
+    public interface IHttpCredentialBase : IHasToken
+    {
+        /// <summary>
+        /// A token containing the api username
+        /// </summary>
+        KeyValuePair<string, string> Id { get; }
+    }
+}
