83fbcbf58d5aed790818921260d0d511af218890
Eli <elias.styner@gmail.com>
2024-11-20 13:52:54 +0100

    changed search result functionality

--- Codespirals.Base.Abstractions/Interfaces/EnforcementInterfaces/ISearchResult.cs
+++ Codespirals.Base.Abstractions/Interfaces/EnforcementInterfaces/ISearchResult.cs
@@ -3,12 +3,18 @@
     /// <summary>
     /// The result from an <see cref="ISearch"/> query and all data necessary to implement pagination
     /// </summary>
-    /// <typeparam name="T">The type of the search result items</typeparam>
-    public interface ISearchResult<T> : IPagination
+    /// <typeparam name="TResult">The type of the search result items</typeparam>
+    public interface ISearchResult<TSearch, TResult>
+        where TSearch : ISearch
     {
+        TSearch Search {  get; } 
+        /// <summary>
+        /// The total number of search results that matched the parameters
+        /// </summary>
+        int TotalResults { get; }
         /// <summary>
         /// The returned search results as filtered by the <see cref="ISearch"/> conditions
         /// </summary>
-        ICollection<T> Results { get; }
+        ICollection<TResult> Results { get; }
     }
 }
---
+++ Codespirals.Base.Extensions/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Codespirals.Base.Extensions.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> list) => list.Where(i => i is not null)!;
+    }
+}
