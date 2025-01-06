4176e5190f04066bc231b87d9f93ad2920c02765
Eli <elias.styner@gmail.com>
2025-01-06 23:01:51 +0100

    updated dependencies

--- Codespirals.Base.Abstractions/Interfaces/EnforcementInterfaces/IHasCurrency.cs
+++ Codespirals.Base.Abstractions/Interfaces/EnforcementInterfaces/IHasCurrency.cs
@@ -1,9 +1,5 @@
 ﻿namespace Codespirals.Base
 {
-    /// <summary>
-    /// Unifying Currency in its own interface removes ambiguity
-    /// from items that use both <seealso cref="ITotal"/> and <seealso cref="IMoneyItem"/> interfaces
-    /// </summary>
     public interface IHasCurrency<TCurrency>
         where TCurrency : ICurrencyBase
     {
