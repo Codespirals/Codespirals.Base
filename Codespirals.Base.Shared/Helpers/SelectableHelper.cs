b1e134827b9232de8065764305270eb7f51122fe
Eli <elias.styner@gmail.com>
2025-01-29 18:42:31 +0100

    build and package updates

--- Codespirals.Base.Shared/Helpers/SelectableHelper.cs
+++ Codespirals.Base.Shared/Helpers/SelectableHelper.cs
@@ -6,20 +6,27 @@
             where TSelectable : ISelectableBase, new()
             where TSelectableEnum : IIsEnum<TSelectable>, new()
         {
-            var list = new List<TSelectable>();
-            var allowableValues = new TSelectableEnum();
-            var properties = typeof(TSelectableEnum).GetProperties();
-            foreach (var prop in properties)
+            try
             {
-                var propValue = prop.GetValue(allowableValues);
-                if (propValue is null)
-                    break;
-                if (propValue is ISelectableBase cast)
+                var list = new List<TSelectable>();
+                var allowableValues = new TSelectableEnum();
+                var properties = typeof(TSelectableEnum).GetProperties();
+                foreach (var prop in properties)
                 {
-                    list.Add(new TSelectable { Id = cast.Id, Name = cast.Name, Description = cast.Description });
+                    var propValue = prop.GetValue(allowableValues);
+                    if (propValue is null)
+                        continue;
+                    if (propValue is ISelectableBase cast)
+                    {
+                        list.Add(new TSelectable { Id = cast.Id, Name = cast.Name, Description = cast.Description });
+                    }
                 }
+                return [.. list];
             }
-            return [.. list];
+            catch (Exception)
+            {
+                return [];
+            }
         }
     }
 }
