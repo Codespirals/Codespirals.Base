a513d5612dd23ede2b68786a927c61bc8ddff1cd
Eli <elias.styner@gmail.com>
2025-01-30 01:08:01 +0100

    updating tests

--- Codespirals.Base.Shared/Extensions/ListExtensions.cs
+++ Codespirals.Base.Shared/Extensions/ListExtensions.cs
@@ -3,5 +3,19 @@
     public static class ListExtensions
     {
         public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> list) => list.Where(i => i is not null)!;
+
+        private static readonly Random rng = new();
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
     }
 }
