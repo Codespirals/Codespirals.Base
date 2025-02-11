99ff4481202d6c0126f628a1e29b8ce3eb7ea6fa
Eli <elias.styner@gmail.com>
2025-02-12 00:47:33 +0100

    worked on razor components

--- Codespirals.Base.Frontend/Resources/ComponentText.Designer.cs
+++ Codespirals.Base.Frontend/Resources/ComponentText.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Codespirals.Base.Shared.Resources.Text {
+namespace Codespirals.Base.Frontend.Resources {
     using System;
     
     
@@ -22,14 +22,14 @@ namespace Codespirals.Base.Shared.Resources.Text {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class BaseText {
+    internal class ComponentText {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal BaseText() {
+        internal ComponentText() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Codespirals.Base.Shared.Resources.Text {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Codespirals.Base.Shared.Resources.Text.BaseText", typeof(BaseText).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Codespirals.Base.Frontend.Resources.ComponentText", typeof(ComponentText).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
