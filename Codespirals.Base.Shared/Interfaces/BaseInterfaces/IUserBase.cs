23a9ce8cc3de143fcab000c1c9ea115cfc2ca285
Eli <elias.styner@gmail.com>
2025-02-24 00:08:52 +0100

    added and updated mock project

--- Codespirals.Base.Shared/Interfaces/BaseInterfaces/IUserBase.cs
+++ Codespirals.Base.Shared/Interfaces/BaseInterfaces/IUserBase.cs
@@ -1,7 +1,13 @@
 ﻿namespace Codespirals.Base
 {
-    public interface IUserBase : IIdentifiable, IHasUsername, IHasPronouns
+    public interface IUserBase : IIdentifiable, IHasUsername
     {
-
+        /// <summary>
+        /// A set of personal pronouns
+        /// </summary>
+        /// <example>He/Him</example>
+        /// <example>She/Her</example>
+        /// <example>They/Them</example>
+        public string? Pronouns { get; }
     }
 }
