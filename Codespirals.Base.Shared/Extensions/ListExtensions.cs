1e26bd0d0586025f51900ae92fc04cf53147f129
Eli <elias.styner@gmail.com>
2025-03-01 19:39:29 +0100

    changed random implementations to shared instance

--- Codespirals.Base.Shared/Extensions/ListExtensions.cs
+++ Codespirals.Base.Shared/Extensions/ListExtensions.cs
@@ -4,14 +4,13 @@
     {
         public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> list) => list.Where(i => i is not null)!;
 
-        private static readonly Random rng = new();
         public static void Shuffle<T>(this IList<T> list)
         {
             int n = list.Count;
             while (n > 1)
             {
                 n--;
-                int k = rng.Next(n + 1);
+                int k = Random.Shared.Next(n + 1);
                 (list[n], list[k]) = (list[k], list[n]);
             }
         }
--- Codespirals.Base.Shared/Extensions/StringExtensions.cs
+++ Codespirals.Base.Shared/Extensions/StringExtensions.cs
@@ -2,11 +2,10 @@
 {
     public static class StringExtensions
     {
-        private static readonly Random random = new();
         public static string GenerateRandomStringFromCharacters(this string allowedCharacters, int length, bool capitalizeOnlyFirstLetter = false)
         {
             var s = new string(Enumerable.Repeat(allowedCharacters, length)
-                .Select(s => s[random.Next(s.Length)]).ToArray());
+                .Select(s => s[Random.Shared.Next(s.Length)]).ToArray());
             if (capitalizeOnlyFirstLetter)
                 return $"{s.First().ToString().ToUpper()}{s[1..].ToLower()}";
             else
