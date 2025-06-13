a4e85642fafc55636ca84ae2b964ea4715ecfa18
Eli <elias.styner@gmail.com>
2025-06-13 17:23:06 +0200

    reworked results a bit again

--- Codespirals.Base.Shared/Interfaces/EnforcementInterfaces/IResult.cs
+++ Codespirals.Base.Shared/Interfaces/EnforcementInterfaces/IResult.cs
@@ -1,13 +1,18 @@
 ﻿namespace Codespirals.Base
 {
-    public interface IResult
+    public interface IResult<TSelf>
+        where TSelf : IResult<TSelf>
     {
         public bool Success { get; }
-        public int ErrorCode { get; set; }
+        public int ErrorCode { get; }
         public string Error { get; }
+        public TSelf OK();
+        public TSelf Fail(int errocCode, string error);
     }
-    public interface IResult<TData> : IResult
+    public interface IResult<TSelf, TData> : IResult<TSelf>
+        where TSelf : IResult<TSelf, TData>
     {
         public TData? Data { get; }
+        public TSelf OK(TData data);
     }
 }
