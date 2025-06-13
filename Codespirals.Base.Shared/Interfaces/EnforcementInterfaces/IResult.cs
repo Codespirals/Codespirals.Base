fc8d6c6a1ac925a7a00a19e8c83d2e9ec3469462
Eli <elias.styner@gmail.com>
2025-06-13 11:10:20 +0200

    updated result interface

--- Codespirals.Base.Shared/Interfaces/EnforcementInterfaces/IResult.cs
+++ Codespirals.Base.Shared/Interfaces/EnforcementInterfaces/IResult.cs
@@ -1,8 +1,13 @@
 ﻿namespace Codespirals.Base
 {
-    public interface IResult<TData, TError>
+    public interface IResult
     {
-        public TError? Error { get; }
+        public bool Success { get; }
+        public int ErrorCode { get; set; }
+        public string Error { get; }
+    }
+    public interface IResult<TData> : IResult
+    {
         public TData? Data { get; }
     }
 }
