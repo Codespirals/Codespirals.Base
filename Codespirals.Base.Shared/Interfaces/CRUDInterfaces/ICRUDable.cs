dc78ec4c25444223a4224572e928f6b80a33e171
Eli <elias.styner@gmail.com>
2025-06-26 15:18:28 +0200

    messed up when unifying the order of parameters for crud operations

--- Codespirals.Base.Shared/Interfaces/CRUDInterfaces/ICRUDable.cs
+++ Codespirals.Base.Shared/Interfaces/CRUDInterfaces/ICRUDable.cs
@@ -8,12 +8,12 @@
     /// <typeparam name="TReturn">The read or get type</typeparam>
     /// <typeparam name="TEdit">The update or edit type</typeparam>
     /// <typeparam name="TDeleteReturn">What's expected to be returned by a delete call</typeparam>
-    public interface ICRUDable<TReturn, TCreate, TEdit, TDeleteReturn, TId> : ICreatable<TCreate, TReturn>, IRetrievable<TReturn, TId>, IUpdatable<TEdit, TReturn>, IDeletable<TDeleteReturn, TId>
+    public interface ICRUDable<TReturn, TCreate, TEdit, TDeleteReturn, TId> : ICreatable<TReturn, TCreate>, IRetrievable<TReturn, TId>, IUpdatable<TReturn, TEdit>, IDeletable<TDeleteReturn, TId>
     {
 
     }
     /// <inheritdoc/>
-    public interface ICRUDableAsync<TReturn, TCreate, TEdit, TDeleteReturn, TId> : ICreatableAsync<TCreate, TReturn>, IRetrievableAsync<TReturn, TId>, IUpdatableAsync<TEdit, TReturn>, IDeletableAsync<TDeleteReturn, TId>
+    public interface ICRUDableAsync<TReturn, TCreate, TEdit, TDeleteReturn, TId> : ICreatableAsync<TReturn, TCreate>, IRetrievableAsync<TReturn, TId>, IUpdatableAsync<TReturn, TEdit>, IDeletableAsync<TDeleteReturn, TId>
     {
 
     }
