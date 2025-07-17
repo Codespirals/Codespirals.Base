145813343255aeb1cd3cb9f9776a472834069d6d
Eli Laptop <elias.styner@gmail.com>
2025-07-17 07:52:40 +0200

    added advanced method to email sender service

--- Codespirals.Base.Shared/Interfaces/CommonServices/IEmailSenderService.cs
+++ Codespirals.Base.Shared/Interfaces/CommonServices/IEmailSenderService.cs
@@ -16,5 +16,20 @@
         /// <param name="to">The recipient as a comma separated string</param>
         /// <returns></returns>
         public Task<TResult> SendEmailAsync(string from, string subject, string body, string to);
+
+    }
+    public interface IEmailSenderService<TResult, TUser> : IEmailSenderService<TResult>
+        where TResult : IResult
+        where TUser : IUserBase, IHasEmail
+    {
+        /// <summary>
+        /// Send an email
+        /// </summary>
+        /// <param name="from">The user that sends the email</param>
+        /// <param name="subject">The general subject of the email</param>
+        /// <param name="body">The text of the email</param>
+        /// <param name="to">The recipients as a list of users</param>
+        /// <returns></returns>
+        public Task<TResult> SendEmailAsync(TUser from, string subject, string body, IEnumerable<TUser> to);
     }
 }
