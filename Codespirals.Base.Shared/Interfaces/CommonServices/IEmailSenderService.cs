8b4f4953c794d99bcfe0b0c31f7a02ac5d0ba6b7
Eli Laptop <elias.styner@gmail.com>
2025-07-17 07:28:40 +0200

    simplified service

--- Codespirals.Base.Shared/Interfaces/CommonServices/IEmailSenderService.cs
+++ Codespirals.Base.Shared/Interfaces/CommonServices/IEmailSenderService.cs
@@ -16,15 +16,5 @@
         /// <param name="to">The recipient as a comma separated string</param>
         /// <returns></returns>
         public Task<TResult> SendEmailAsync(string from, string subject, string body, string to);
-
-        /// <summary>
-        /// Send an email
-        /// </summary>
-        /// <param name="from">The address that sends the email</param>
-        /// <param name="subject">The general subject of the email</param>
-        /// <param name="body">The text of the email</param>
-        /// <param name="to">The recipient(s) as a comma separated string</param>
-        /// <returns>A result showing if the entire operation succeeded, as well as the success for every sent mail.</returns>
-        public Task<Result<List<TResult>>> SendEmailToManyAsync(string from, string subject, string body, string to);
     }
 }
