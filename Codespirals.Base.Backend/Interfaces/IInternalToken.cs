812e68459b4cece834bda7354a811987be057408
Eli <elias.styner@gmail.com>
2025-09-13 15:48:55 +0200

    cleanup and move from local folder to onedrive

--- Codespirals.Base.Backend/Interfaces/IInternalToken.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Codespirals.Base
-{
-    public interface IInternalToken<TType> : ITokenBase, ICreatable
-    {
-        public int InvalidAttempts { get; }
-        public int? MaxInvalidAttempts { get; }
-    }
-}
