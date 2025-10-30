384a2b0610939c1de8a326437915747f99b1f446
Eli <elias.styner@gmail.com>
2025-10-30 11:57:14 +0100

    added attribute

---
+++ Codespirals.Base.Shared/Attributes/RequiredInjectableService.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace Codespirals.Base;
+
+[AttributeUsage(AttributeTargets.Class,
+    AllowMultiple = true,
+    Inherited = true)]
+public class RequiredInjectableService<TService>(TService service, string? key = null) : Attribute
+{
+    public TService Service { get; internal set; } = typeof(TService).GetCustomAttributes<InjectableService>().Any() ? service : throw new Exception($"Service {service?.GetType().Name ?? "[Name not found]"} does not implement the attribute {nameof(InjectableService)}.");
+    public string? Key { get; internal set; } = key;
+}
