3eee64155e2abe4ee69f5ac79e1315b5d747f4e9
Eli <elias.styner@gmail.com>
2025-12-05 19:30:28 +0100

    fixed an incompatible property

--- Codespirals.Base.Shared/Attributes/RequiredInjectableService.cs
+++ Codespirals.Base.Shared/Attributes/RequiredInjectableService.cs
@@ -6,7 +6,7 @@ namespace Codespirals.Base.Attributes;
 [AttributeUsage(AttributeTargets.Class,
     AllowMultiple = true,
     Inherited = true)]
-public sealed class RequiredInjectableService(Type service, ServiceLifetime? lifetime = null, string? key = null) : Attribute
+public sealed class RequiredInjectableService(Type service, ServiceLifetime lifetime = ServiceLifetime.Scoped, string? key = null) : Attribute
 {
     public Type Service { get; internal set; } = service.GetCustomAttributes<InjectableService>().Any() ? service : throw new Exception($"Service {service?.Name ?? "[Name not found]"} does not implement the attribute {nameof(InjectableService)}.");
     public ServiceLifetime? Lifetime { get; internal set; } = lifetime;
