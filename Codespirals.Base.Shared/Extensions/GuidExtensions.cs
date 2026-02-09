35cbbbcabf9c0327ecf4de19a4c353e1f38f93a9
Eli <elias.styner@gmail.com>
2026-02-09 01:31:16 +0100

    added an obscuring extension for guids

--- Codespirals.Base.Shared/Extensions/GuidExtensions.cs
+++ Codespirals.Base.Shared/Extensions/GuidExtensions.cs
@@ -10,6 +10,7 @@ public static class GuidExtensions
     /// </summary>
     /// <param name="id"></param>
     /// <returns></returns>
+    /// <remarks>NOT url safe. This action is reversible with <see cref="Helpers.GuidHelper.FromBase64(string)"/></remarks>
     public static string ToBase64(this Guid id)
         => Convert.ToBase64String(id.ToByteArray());
 
@@ -21,4 +22,13 @@ public static class GuidExtensions
     /// <remarks>This action is deterministic, but  not reversible</remarks>
     public static string ToBase64UrlSafe(this Guid id)
         => id.ToBase64().Trim('=').MakeUrlSafe();
+
+    /// <summary>
+    /// Convert a <see cref="Guid"/> into a shorter, url safe string starting with a max 4 character prefix that marks it as obscure
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="marker">Max 4 character prefix to identify this as obscured.</param>
+    /// <returns></returns>
+    public static string ToObscureId(this Guid id, string marker)
+        => $"{marker[..4].MakeUrlSafe}{id.ToBase64().Trim('=').MakeUrlSafe()}";
 }
--- Codespirals.Base.Shared/Helpers/GuidHelper.cs
+++ Codespirals.Base.Shared/Helpers/GuidHelper.cs
@@ -10,5 +10,14 @@ public static class GuidHelper
     /// <param name="base64String">The base64 encoded string to create the GUID from</param>
     /// <returns></returns>
     public static Guid FromBase64(string base64String)
-        => new(Convert.FromBase64String(base64String[..22].PadRight(24, '='))[..16]);
+    {
+        try
+        {
+            return new(Convert.FromBase64String(base64String[..22].PadRight(24, '='))[..16]);
+        }
+        catch (Exception)
+        {
+            return Guid.Empty;
+        }
+    }
 }
