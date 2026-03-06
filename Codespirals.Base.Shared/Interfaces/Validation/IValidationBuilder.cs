1f79bd8eec83163513804ecdaee5077213d05519
elias <elias.styner@gmail.com>
2026-03-06 16:05:24 +0100

    added a skeleton interface for validation.

---
+++ Codespirals.Base.Shared/Interfaces/Validation/IValidationBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Codespirals.Base.Results;
+namespace Codespirals.Base.Validation;
+
+public interface IValidationBuilder<TValidationResult, TErrorCode>
+    where TValidationResult : IResult<TErrorCode>
+{
+    public TValidationResult Validate();
+}
+
+public interface IValidationBuilderAsync<TValidationResult, TErrorCode>
+    where TValidationResult : IResult<TErrorCode>
+{
+    public Task<TValidationResult> Validate();
+}
---
+++ Codespirals.Base.Shared/Interfaces/Validation/IValidator.cs
@@ -0,0 +1,10 @@
+﻿using Codespirals.Base.Results;
+
+namespace Codespirals.Base.Validation;
+
+public interface IValidator<TValidationBuilder, TValidationResult, TErrorCode>
+    where TValidationBuilder : IValidationBuilder<TValidationResult, TErrorCode>, new()
+    where TValidationResult : IResult<TErrorCode>
+{
+    public TValidationBuilder BeginValidation<TAdditionalData>();
+}
