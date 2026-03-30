4004b5880c323e95fbf85244b1aff864952cf90a
Eli <elias.styner@gmail.com>
2026-03-30 22:42:06 +0200

    added time constants to base

---
+++ Codespirals.Base.Shared/Constants/TimeConstants.cs
@@ -0,0 +1,44 @@
+﻿namespace Codespirals.Base;
+
+/// <summary>
+/// 
+/// </summary>
+public static class TimeConstants
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public const int SecondsPerHour = 3600;
+    /// <summary>
+    /// 
+    /// </summary>
+    public const int SecondsPerDay = 86400;
+    /// <summary>
+    /// 
+    /// </summary>
+    public const int SecondsPerWeek = 604800;
+    /// <summary>
+    /// Seconds per 30 day period
+    /// </summary>
+    public const int SecondsPerMonth = 2592000;
+    /// <summary>
+    /// 
+    /// </summary>
+    public const int MinutesPerDay = 1440;
+    /// <summary>
+    /// 
+    /// </summary>
+    public const int MinutesPerWeek = 10080;
+    /// <summary>
+    /// Minutes per 30 day period
+    /// </summary>
+    public const int MinutesPerMonth = 43200;
+    /// <summary>
+    /// 
+    /// </summary>
+    public const int HoursPerWeek = 168;
+    /// <summary>
+    /// Hours per 30 day period
+    /// </summary>
+    public const int HoursPerMonth = 720;
+}
