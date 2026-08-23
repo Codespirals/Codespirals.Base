7ffaff9126ef30c395753fe5e3fbb0fcc26f099d
Elias-Styner <elias.styner@gmail.com>
2026-08-23 21:05:59 +0200

    updated to .net10

--- Codespirals.Base.Shared/Extensions/LoggingExtensions.cs
+++ Codespirals.Base.Shared/Extensions/LoggingExtensions.cs
@@ -11,18 +11,18 @@ public static partial class LoggingExtensions
     /// Start a logging session
     /// </summary>
     /// <param name="logger"></param>
-    /// <param name="method"></param>
+    /// <param name="function"></param>
     /// <param name="additionalArguments">A list of arguments to be added to the scope</param>
     /// <param name="message"></param>
     /// <returns></returns>
-    public static IDisposable? BeginLog(this ILogger logger, string method, string? message = null, params (string, string)[] additionalArguments)
+    public static IDisposable? BeginLog(this ILogger logger, string function, string? message = null, params (string, string)[] additionalArguments)
     {
         try
         {
-            var processId = Guid.NewGuid().ToString();
-            var scope = string.Join('\n', BuildScope(processId, method, additionalArguments).Select(x => $"{x.Key}:{x.Value}"));
-            logger.LogStep(State.Started, $"{scope}\n{message ??= "Beginning new log"}");
-            return logger.BeginScope(scope);
+            var scopeString = string.Join('\n', BuildScope(function, additionalArguments).Select(x => $"{x.Key}:{x.Value}"));
+            var scope = logger.BeginScope(scopeString);
+            logger.LogStep(State.Started, message ??= "Beginning new log"); 
+            return scope;
         }
         catch (Exception ex)
         {
@@ -47,13 +47,13 @@ public static partial class LoggingExtensions
     /// <param name="exception"></param>
     public static void LogException(this ILogger logger, State state, Exception exception)
         => logger.LogCritical(exception, "Exception triggered! Current state of operation: {state}\r\n{message}", state, exception.Message);
-    private static Dictionary<string, string> BuildScope(string processId, string service, params (string, string)[] additionalArguments)
+    private static Dictionary<string, string> BuildScope(string function, params (string, string)[] additionalArguments)
     {
         Dictionary<string, string> scopeItems = new()
         {
-            { "ProcessId", processId }
+            { "ProcessId", Guid.NewGuid().ToString() }
         };
-        if (!string.IsNullOrWhiteSpace(service)) { scopeItems.Add("Method", nameof(service)); }
+        if (!string.IsNullOrWhiteSpace(function)) { scopeItems.Add("Method", nameof(function)); }
         if (additionalArguments is not null)
             foreach (var argument in additionalArguments)
                 if (!string.IsNullOrWhiteSpace(argument.Item1) && !string.IsNullOrWhiteSpace(argument.Item2)) { scopeItems.Add(argument.Item1, argument.Item2); }
